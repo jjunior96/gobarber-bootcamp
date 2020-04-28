23d6891c601456e5ddc170b4260c22ff7da30afe
Junior Alves <jjalves96@gmail.com>
2020-04-28 09:40:30 -0300

    feat: User update & verifications

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/authMiddleware';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Privates routes
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
